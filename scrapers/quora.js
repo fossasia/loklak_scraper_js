24f59dd5f9fd216d57312d42cc562f3f10dd9fd3
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-06-07 10:58:11 +0530

    fixes #26 - Updated quora profile scraper to use base.js
    fixes issue #26, updated quora profile scraper to use base.js and
    also enhanced it to make it at par with the loklak server quora
    scraper

--- scrapers/quora.js
+++ scrapers/quora.js
@@ -1,51 +1,72 @@
-var request = require("request");
-var cheerio = require("cheerio");
+const BaseLoklakScrapper = require('./base');
 
-var profile = null;
-var QUORA_URL_BASE = "https://www.quora.com/profile/";
-var html = "";
-var quoraProfile = {};
-var $ = null;
+class QuoraLoklakScrapper extends BaseLoklakScrapper {
 
-if (process.argv.length <= 2) {
-  console.log("Atleast one argument required");
-  process.exit(-1);
-}
-
-if (process.argv.length === 3) {
-  profile = process.argv[2];
-}
-
-function scrapeQuora() {
-  $ = cheerio.load(html);
-
-  var bio = $(".ProfileDescription").text();
-  quoraProfile["bio"] = bio;
-
-  var profileImage = $(".profile_photo_img").attr("src");
-  quoraProfile["profileImage"] = profileImage;
-
-  var userName = $(".profile_photo_img").attr("alt");
-  quoraProfile["user"] = userName;
-
-  var rssFeed = QUORA_URL_BASE + profile + "/rss";
-  quoraProfile["rss_feed_link"] = rssFeed;
-
-  var stats = $(".list_count");
-  
-  for (var i = 0;i < stats.length; i++) {
-    var stat=stats[i];
-    quoraProfile[stat.prev.data.toLowerCase().trim() + "Url"] = "https://www.quora.com" + stat.parent.attribs.href;
-    quoraProfile[stat.prev.data.toLowerCase().trim() + "Count"] = stat.children[0].data; 
+  constructor() {
+    super('Quora', 'https://www.quora.com');
   }
-  console.log(quoraProfile);
+
+  argumentSanityCheck(args) {
+    super.argumentSanityCheck(args);
+
+    if (args.length <= 2) {
+      console.error('Atleast one argument required.');
+      process.exit(-1);
+    }
+
+    return true;
+  }
+
+  onInit() {
+    this.REQUEST_URL = this.BASE_URL + "/profile/" + this.SLICED_PROC_ARGS[0];
+    this.request();
+  }
+
+  scrape($) {
+    const quoraProfile = {};
+    const feed = {};
+    const topics = [];
+
+    quoraProfile["profile_url"] = this.REQUEST_URL;
+    quoraProfile["bio"] = $(".ProfileDescription").text();
+    quoraProfile["profile_image"] = $(".profile_photo_img").attr("src");
+    quoraProfile["user_name"] = $(".profile_photo_img").attr("alt");
+    const rssFeedLink = this.BASE_URL + "/profile/" + this.SLICED_PROC_ARGS[0] + "/rss";
+    quoraProfile["rss_feed_link"] = rssFeedLink;
+
+    var stats = $(".list_count");
+    for (var i = 0;i < stats.length; i++) {
+      var stat=stats[i];
+      feed[stat.prev.data.toLowerCase().trim() + "_url"] = this.BASE_URL + stat.parent.attribs.href;
+      feed[stat.prev.data.toLowerCase().trim() + "_count"] = stat.children[0].data;
+    }
+    quoraProfile["feed"] = feed;
+
+    $(".IdentityCredential").each((i, elem) => {
+      var infoText = $(elem).text();
+      if (infoText.substring(0, 5) === "Studi") {
+        quoraProfile[infoText.split(" ")[0].toLowerCase().trim() + "_at"] = infoText;
+      } else if (infoText.substring(0, 5) === "Lives") {
+        quoraProfile["lives_in"] = infoText;
+      } else {
+        quoraProfile["works_at"] = infoText;
+      }
+    });
+
+    $(".TopicName").each((i, elem) => {
+      topics.push($(elem).text());
+    });
+    quoraProfile["knows_about"] = topics;
+
+    this.JSON = quoraProfile;
+
+    // Uncomment the following line for testing
+    // console.log(quoraProfile);
+
+    return this.JSON;
+  }
 }
 
-request(QUORA_URL_BASE + profile, function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-    process.exit(-1);
-  }
-  html = body;
-  scrapeQuora();
-});
+module.exports = QuoraLoklakScrapper;
+
+new QuoraLoklakScrapper();

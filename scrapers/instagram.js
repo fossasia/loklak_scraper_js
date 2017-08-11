4b2f6eae41bcb28d42be5411cd4ff10a60cba74b
Mohd Abdullah <ism2014004@iiita.ac.in>
2017-08-11 22:27:30 +0530

    fixes #50 - extended base.js and improved data format

--- scrapers/instagram.js
+++ scrapers/instagram.js
@@ -1,43 +1,61 @@
 /* Usage : node instagram.js <profile_name>
- */
+*/
 
-var request = require("request");
-var cheerio = require("cheerio");
+const BaseLoklakScrapper = require('./base');
 
 var profile = null;
 
-// profile name must be provided as a command line argument
+class instagramScrapper extends BaseLoklakScrapper
+{
+	constructor()
+	{
+		super('Instagram','http://www.instagram.com/');
+	}
 
-if (process.argv.length <= 2) {
-  console.log("Atleast one argument required");
-  process.exit(-1);
+	argumentSanityCheck(args) 
+	{
+		super.argumentSanityCheck(args);
+
+		if (args.length <= 2) 
+		{
+			console.error('Atleast one argument required.');
+			process.exit(-1);
+		}
+
+		return true;
+	}
+
+	onInit() {
+		this.REQUEST_URL = this.BASE_URL + this.SLICED_PROC_ARGS[0];
+		this.request();
+	}
+
+	scrape($)
+	{
+		profile=this.REQUEST_URL;
+		var instaBody=this.HTML;
+		var pos1=instaBody.indexOf("window._sharedData = ");
+		var pos2=instaBody.indexOf("</script>",pos1);
+		var finalContent=instaBody.slice(pos1+21,pos2-1);
+		var jsonParsedContent=JSON.parse(finalContent); 
+		this.JSON=jsonParsedContent;
+		var keys=Object.keys(jsonParsedContent);
+		//uncomment the line below to display JSON keys
+		//console.log(keys); 
+		//uncomment the function below to display some common details
+		this.display(jsonParsedContent); 
+		return this.JSON;
+	}
+
+	display(jsonParsedContent)
+	{
+		console.log(jsonParsedContent["activity_counts"]);
+		console.log(jsonParsedContent["country_code"]);
+		console.log(jsonParsedContent["language_code"]);
+		var profileDetails=jsonParsedContent["entry_data"]["ProfilePage"];
+		console.log(profileDetails[0]["user"]);
+	}
 }
 
-profile = process.argv[2];
-
-var html = null;
-var blogPosts = [];
-var $ = null;
-var INSTAGRAM_BASE_URL = "https://www.instagram.com/";
-
-// getting the instagram web page
-
-request(INSTAGRAM_BASE_URL + profile, function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-    process.exit(-1);
-  }
-  html = body;
-  scrapeInstagram()
-});
-
-// function to scrape instagram profile information
-
-function scrapeInstagram() {
-  $ = cheerio.load(html);
-
-  var instaObj = $("script").eq(3).html().trim().substring(21);
-  var instaProfile = [];
-  instaProfile.push(instaObj);
-  console.log(instaProfile);
-}
+module.exports=instagramScrapper;
+new instagramScrapper();
